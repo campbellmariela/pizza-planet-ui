d25b593c288986663da157aa4cb9088cc85aa808
Mariela Campbell <121921050+campbellmariela@users.noreply.github.com>
2024-08-14 02:38:10 -0300

    PIZ2-add-a-beverage-model-manager-and-service (#1)
    feat/MC: PIZ2 - addition of beverages when making and viewing an order, creation of beverages section with update and create functionality

--- js/index.js
+++ js/index.js
@@ -14,8 +14,6 @@ function postOrder(order) {
     })
         .then(res => res.json())
         .then(res => showNotification());
-
-
 }
 
 /**
@@ -36,8 +34,12 @@ orderForm.submit(event => {
  */
 function getOrderData() {
     let ingredients = [];
+    let beverages = [];
     $.each($("input[name='ingredients']:checked"), function (el) {
         ingredients.push($(this).val());
+    });
+    $.each($("input[name='beverages']:checked"), function (el) {
+        beverages.push($(this).val());
     });
 
     return {
@@ -46,7 +48,8 @@ function getOrderData() {
         client_address: $("input[name='address']").val(),
         client_phone: $("input[name='phone']").val(),
         size_id: $("input[name='size']:checked").val(),
-        ingredients
+        ingredients,
+        beverages
     };
 }
 
@@ -59,15 +62,23 @@ function showNotification() {
     setTimeout(() => orderAlert.toggle(), 5000);
 }
 
-
 // Gather information in a dynamic way
-
 function fetchIngredients() {
     fetch('http://127.0.0.1:5000/ingredient/')
         .then(response => response.json())
         .then(ingredients => {
             let rows = ingredients.map(element => createIngredientTemplate(element));
             let table = $("#ingredients tbody");
+            table.append(rows);
+        });
+}
+
+function fetchBeverages() {
+    fetch('http://127.0.0.1:5000/beverage/')
+        .then(response => response.json())
+        .then(beverages => {
+            let rows = beverages.map(element => createBeverageTemplate(element));
+            let table = $("#beverages tbody");
             table.append(rows);
         });
 }
@@ -87,6 +98,11 @@ function createIngredientTemplate(ingredient) {
     return Mustache.render(template, ingredient);
 }
 
+function createBeverageTemplate(beverage) {
+    let template = $("#beverages-template")[0].innerHTML;
+    return Mustache.render(template, beverage);
+}
+
 function createSizeTemplate(size) {
     let template = $("#sizes-template")[0].innerHTML;
     return Mustache.render(template, size);
@@ -94,6 +110,7 @@ function createSizeTemplate(size) {
 
 function loadInformation() {
     fetchIngredients();
+    fetchBeverages();
     fetchOrderSizes();
 }
 
